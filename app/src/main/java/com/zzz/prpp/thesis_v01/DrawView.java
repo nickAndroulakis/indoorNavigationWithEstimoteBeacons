143868b3be8a0a28f737117c6738ec3d4f44ec29
Nick Androulakis <nick.androul@gmail.com>
2018-08-31 11:23:32 +0300

    added the Draw View

---
+++ app/src/main/java/com/zzz/prpp/thesis_v01/DrawView.java
@@ -0,0 +1,20 @@
+package com.zzz.prpp.thesis_v01;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawView extends View {
+    public DrawView(Context context) {
+        super(context);
+    }
+
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
--- app/src/main/java/com/zzz/prpp/thesis_v01/NodeRoomDatabase.java
+++ app/src/main/java/com/zzz/prpp/thesis_v01/NodeRoomDatabase.java
@@ -70,5 +70,4 @@ public abstract class NodeRoomDatabase extends RoomDatabase {
             return null;
         }
     }
-
 }
