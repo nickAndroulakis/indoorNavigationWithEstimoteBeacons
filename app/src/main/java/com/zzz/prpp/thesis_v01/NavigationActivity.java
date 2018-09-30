b7fd3a4c5172a5661398debd78719753969e67b7
Nick Androulakis <nick.androul@gmail.com>
2018-09-30 20:01:03 +0300

    added calculateClosestNode in NavigationActivity.

--- app/src/main/java/com/zzz/prpp/thesis_v01/NavigationActivity.java
+++ app/src/main/java/com/zzz/prpp/thesis_v01/NavigationActivity.java
@@ -6,11 +6,32 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.recognition.packets.Beacon;
+import com.estimote.coresdk.service.BeaconManager;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import static com.estimote.coresdk.observation.region.RegionUtils.computeAccuracy;
 
 public class NavigationActivity extends AppCompatActivity {
 
     private NodeViewModel mNodeViewModel;
+
+    private BeaconManager beaconManager;
+    private BeaconRegion region;
+    public Integer closestNodeNumber;
+
+    private Double one = 100.0;
+    private Double two = 100.0;
+    private Double three = 100.0;
+    private Double four = 100.0;
+    private Double five = 100.0;
+    private Double six = 100.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +52,66 @@ public class NavigationActivity extends AppCompatActivity {
                 vw.setCoordinates(coordinates);
             }
         });
+
+        region = new BeaconRegion("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.BeaconRangingListener() {
+            @Override
+            public void onBeaconsDiscovered(BeaconRegion region, List<Beacon> list) {
+                if (!list.isEmpty()) {
+                    clear();
+                    for (Beacon item : list) {
+                        switch (item.getMinor()) {
+                            case 2224:
+                                one = computeAccuracy(item);
+                                break;
+                            case 46152:
+                                two = computeAccuracy(item);
+                                break;
+                            case 41111:
+                                three = computeAccuracy(item);
+                                break;
+                            case 48918:
+                                four = computeAccuracy(item);
+                                break;
+                            case 48677:
+                                five = computeAccuracy(item);
+                                break;
+                        }
+                    }
+                }
+            }
+        });
+
+        Node myNode = new Node(99,one,two,three,four,five, null);
+        closestNodeNumber = calculateClosestNode(myNode);
+    }
+
+    private Integer calculateClosestNode(Node node){
+        Integer closestNodeId = 0;
+        double minDistance = 100.0;
+        List<Node> nodeList = (List) mNodeViewModel.getAllNodes();
+        for (Node nd : nodeList){
+            double distance = Math.sqrt(Math.pow(node.getMOne()-nd.getMOne(),2)+Math.pow(node.getMTwo()-nd.getMTwo(),2)+Math.pow(node.getMThree()-nd.getMThree(),2)+Math.pow(node.getMFour()-nd.getMFour(),2)+Math.pow(node.getMFive()-nd.getMFive(),2));
+            if (distance < minDistance) {closestNodeId = nd.getMNumber();}
+        }
+        return closestNodeId;
+    }
+
+    private void clear() {
+        one = 100.0;
+        two = 100.0;
+        three = 100.0;
+        four = 100.0;
+        five = 100.0;
+        six = 100.0;
+    }
+
+    public static double round(double value) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
